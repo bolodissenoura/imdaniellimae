e5870f8695bef2a90e85983b785b8674dbdc1137
daniellimae <Daniel.Silva.ext@cyrela.com.br>
2022-12-27 22:55:10 -0300

    feat: shaders from vercel

--- components/HomeContent/homeContent.component.tsx
+++ components/HomeContent/homeContent.component.tsx
@@ -6,11 +6,14 @@ export function HomeContent() {
   return (
     <>
       <Styles.Container>
+        <div className="features-gradients">
+          <div className="features-gradient fBall"></div>
+          <div className="features-gradient sBall"></div>
+          <div className="features-gradient tBall"></div>
+        </div>
         <Styles.TextContent>
           <Styles.Title>Daniel limae</Styles.Title>
-          <Styles.Subtitle>
-            Software Developer | Front-end
-          </Styles.Subtitle>
+          <Styles.Subtitle>software developer | front-end</Styles.Subtitle>
         </Styles.TextContent>
       </Styles.Container>
     </>
--- components/HomeContent/homeContent.styles.ts
+++ components/HomeContent/homeContent.styles.ts
@@ -6,6 +6,117 @@ export const Container = styled.div`
   margin: 0px auto;
   width: 800px;
   height: 100vh;
+
+  .fBall {
+    top: 50%;
+    left: 50%;
+    background-color: #5800ff;
+    animation: animation-3 10s infinite;
+  }
+
+  .sBall {
+    top: 100;
+    left: 70%;
+    background-color: #2192ff;
+    animation: animation-3 10s infinite;
+  }
+
+  .tBall {
+    top: 10;
+    left: 30%;
+    background-color: #e900ff;
+    animation: animation-3 10s infinite;
+  }
+
+  .features-gradients {
+    position: absolute;
+    background-color: rgba(1, 1, 1, 0.25);
+    filter: blur(12em);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .features-gradient {
+    width: 200px;
+    height: 200px;
+    top: 0;
+    position: absolute;
+  }
+
+  @keyframes animation-1 {
+    0% {
+      transform: translateY(-50%) translateX(-50%) translateX(-15%)
+        translateY(10%);
+    }
+    20% {
+      transform: translateY(-50%) translateX(-50%) translateX(-20%)
+        translateY(-30%);
+    }
+    40% {
+      transform: translateY(-50%) translateX(-50%) translateX(-25%)
+        translateY(-15%);
+    }
+    60% {
+      transform: translateY(-50%) translateX(-50%) translateX(30%)
+        translateY(20%);
+    }
+    80% {
+      transform: translateY(-50%) translateX(-50%) translateX(5%)
+        translateY(35%);
+    }
+    to {
+      transform: translateY(-50%) translateX(-50%) translateX(-15%)
+        translateY(10%);
+    }
+  }
+  @keyframes animation-2 {
+    0% {
+      transform: translateY(-50%) translateX(-50%) rotate(-20deg)
+        translateX(20%);
+    }
+    25% {
+      transform: translateY(-50%) translateX(-50%) skew(-15deg, 15deg)
+        rotate(-80deg) translateX(30%);
+    }
+    50% {
+      transform: translateY(-50%) translateX(-50%) rotate(180deg)
+        translateX(25%);
+    }
+    75% {
+      transform: translateY(-50%) translateX(-50%) skew(-15deg, 15deg)
+        rotate(240deg) translateX(15%);
+    }
+    to {
+      transform: translateY(-50%) translateX(-50%) rotate(340deg)
+        translateX(20%);
+    }
+  }
+  @keyframes animation-3 {
+    0% {
+      transform: translateY(-50%) translateX(-50%) translateX(-15%)
+        translateY(10%);
+    }
+    20% {
+      transform: translateY(-50%) translateX(-50%) translateX(20%)
+        translateY(-30%);
+    }
+    40% {
+      transform: translateY(-50%) translateX(-50%) translateX(-25%)
+        translateY(-15%);
+    }
+    60% {
+      transform: translateY(-50%) translateX(-50%) translateX(30%)
+        translateY(20%);
+    }
+    80% {
+      transform: translateY(-50%) translateX(-50%) translateX(5%)
+        translateY(35%);
+    }
+    to {
+      transform: translateY(-50%) translateX(-50%) translateX(-15%)
+        translateY(10%);
+    }
+  }
 `;
 
 export const TextContent = styled.div`
@@ -15,25 +126,26 @@ export const TextContent = styled.div`
 `;
 
 export const Title = styled.h1`
-  font-size: 120px;
-  padding: 5px;
-  background: linear-gradient(135deg, #5800ff 0%, #e900ff 100%);
+  font-family: "cartoon";
+  background: linear-gradient(135deg, #f2f2f2 0%, #f2f2f2 100%);
   background-clip: unset;
   -webkit-background-clip: text;
   -moz-background-clip: text;
   -webkit-text-fill-color: transparent;
-  font-family: "Bremlin";
+  padding: 5px;
+  font-size: 120px;
+
   @media (min-width: 600px) {
-    font-size: 60px;
+    font-size: 100px;
   }
 `;
 
 export const Subtitle = styled.p`
-  font-family: "Bremlin";
-  font-size: 38px;
   color: #f2f2f2;
   padding: 8px;
   font-weight: 500;
+  font-size: 16px;
+
   @media (min-width: 600px) {
     font-size: 16px;
   }
