a1969e6e815bc3945c73391e4cd20c8191fa4d7e
daniellima <Daniel.Silva.ext@cyrela.com.br>
2023-05-24 19:04:39 -0300

    feat: components imports

--- components/index.ts
+++ components/index.ts
@@ -3,3 +3,5 @@ export * from "./HomeContent";
 export * from "./LinkButton";
 export * from "./ListsContent";
 export * from "./ButtonComeBack";
+export * from "./Card";
+export * from "./ListsContentLinks";
