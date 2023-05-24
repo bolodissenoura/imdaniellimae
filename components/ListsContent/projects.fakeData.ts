8fe012d156f28ab14413310b2142d2e35d463ff2
daniellima <Daniel.Silva.ext@cyrela.com.br>
2023-05-24 19:06:01 -0300

    fake mock data

---
+++ components/ListsContent/projects.fakeData.ts
@@ -0,0 +1,96 @@
+import { IProjects } from "../../utils/interfaces";
+
+export const dataList: IProjects[] = [
+  {
+    link: "https://vetrin.com.br",
+    title: "Vetrin",
+    imgUrl:
+      "https://cdn.discordapp.com/attachments/1082297301072105544/1111017864187629708/image.png",
+    description:
+      "I created and organized a company that provides services directly to over 20 companies in Brazil.",
+  },
+  {
+    link: "https://draleilawessler.com.br",
+    title: "DRA. LEILA WESSLER - Ginecologista e Obstetra",
+    imgUrl:
+      "https://cdn.discordapp.com/attachments/1082297301072105544/1111017864187629708/image.png",
+    description: "sahuasuhhuas shuauhasuh sahu shuahus ahus",
+  },
+  {
+    link: "https://libertmusic.com.br",
+    title: "Libert Music",
+    imgUrl:
+      "https://cdn.discordapp.com/attachments/1082297301072105544/1111017864187629708/image.png",
+    description: "sahuasuhhuas shuauhasuh sahu shuahus ahus",
+  },
+  {
+    link: "https://bolodissenoura.github.io/pc-jiu/",
+    title: " Team PC Jiu-Jitsu 👊🏼",
+    imgUrl:
+      "https://cdn.discordapp.com/attachments/1082297301072105544/1111017864187629708/image.png",
+    description: "sahuasuhhuas shuauhasuh sahu shuahus ahus",
+  },
+  {
+    link: "https://react-estudo-lom.vercel.app",
+    title: "React Class Consume Api",
+    imgUrl:
+      "https://cdn.discordapp.com/attachments/1082297301072105544/1111017864187629708/image.png",
+    description: "sahuasuhhuas shuauhasuh sahu shuahus ahus",
+  },
+  {
+    link: "https://facil-consulta-front.vercel.app",
+    title: "Form Facil-consulta in VueJS",
+    imgUrl:
+      "https://cdn.discordapp.com/attachments/1082297301072105544/1111017864187629708/image.png",
+    description: "sahuasuhhuas shuauhasuh sahu shuahus ahus",
+  },
+  {
+    link: "https://react-estudo-lom.vercel.app",
+    title: "React Class Consume Api",
+    imgUrl:
+      "https://cdn.discordapp.com/attachments/1082297301072105544/1111017864187629708/image.png",
+    description: "sahuasuhhuas shuauhasuh sahu shuahus ahus",
+  },
+  {
+    link: "https://bolodissenoura.github.io/DateDestino/",
+    title: "Date Destino ❤️",
+    imgUrl:
+      "https://cdn.discordapp.com/attachments/1082297301072105544/1111017864187629708/image.png",
+    description: "sahuasuhhuas shuauhasuh sahu shuahus ahus",
+  },
+  {
+    link: "https://bolodissenoura.github.io/Nutrem/",
+    title: "Nutremp",
+    imgUrl:
+      "https://cdn.discordapp.com/attachments/1082297301072105544/1111017864187629708/image.png",
+    description: "sahuasuhhuas shuauhasuh sahu shuahus ahus",
+  },
+  {
+    link: "https://rifa-na-veia-deploy2-ehgjamhew-bolodissenoura.vercel.app",
+    title: "Next project ( ?? )",
+    imgUrl:
+      "https://cdn.discordapp.com/attachments/1082297301072105544/1111017864187629708/image.png",
+    description: "sahuasuhhuas shuauhasuh sahu shuahus ahus",
+  },
+  {
+    link: "https://bolodissenoura.github.io/Coegi/",
+    title: "Coegi LP",
+    imgUrl:
+      "https://cdn.discordapp.com/attachments/1082297301072105544/1111017864187629708/image.png",
+    description: "sahuasuhhuas shuauhasuh sahu shuahus ahus",
+  },
+  {
+    link: "https://https://cdn.discordapp.com/attachments/1082297301072105544/1111017864187629708/image.pngarp.org.br",
+    title: "AMIGOS DOS ANIMAIS DE RIBEIRÃO PRETO",
+    imgUrl:
+      "https://cdn.discordapp.com/attachments/1082297301072105544/1111017864187629708/image.png",
+    description: "sahuasuhhuas shuauhasuh sahu shuahus ahus",
+  },
+  {
+    link: "https://bolodissenoura.github.io/daniellimae/",
+    title: "Old Portifolio",
+    imgUrl:
+      "https://cdn.discordapp.com/attachments/1082297301072105544/1111017864187629708/image.png",
+    description: "sahuasuhhuas shuauhasuh sahu shuahus ahus",
+  },
+];
